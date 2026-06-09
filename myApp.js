c0dff54b6286a26ac870517525b307714ab7a558
B1STN <b1stncaceres@gmail.com>
2026-06-08 23:21:24 -0400

    Fix MESSAGE_STYLE

--- myApp.js
+++ myApp.js
@@ -1,22 +1,9 @@
-require('dotenv').config();
-
-let express = require('express');
-let app = express();
-
-app.use('/public', express.static(__dirname + '/public'));
-
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
-
 app.get('/json', function(req, res) {
-  let message = 'Hello json';
-
   if (process.env.MESSAGE_STYLE === 'uppercase') {
-    message = message.toUpperCase();
+    res.json({ message: 'HELLO JSON' });
+  } else {
+    res.json({ message: 'Hello json' });
   }
-
-  res.json({ message: message });
 });
 
 module.exports = app;
