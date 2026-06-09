554b5ef43b4c5beae9c159ae31673c1f489af6cc
B1STN <b1stncaceres@gmail.com>
2026-06-08 23:22:47 -0400

    Add env config

--- myApp.js
+++ myApp.js
@@ -1,9 +1,23 @@
 app.get('/json', function(req, res) {
+require('dotenv').config();
+
+let express = require('express');
+let app = express();
+
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/json', function(req, res) {
+  let message = 'Hello json';
+
   if (process.env.MESSAGE_STYLE === 'uppercase') {
-    res.json({ message: 'HELLO JSON' });
-  } else {
-    res.json({ message: 'Hello json' });
+    message = message.toUpperCase();
   }
+
+  res.json({ message: message });
 });
 
 module.exports = app;
