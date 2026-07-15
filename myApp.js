5d842239c8bde3f98c0de2e3a3a348e099b014b2
B1STN <b1stncaceres@gmail.com>
2026-07-15 19:58:39 -0400

    fix express app initialization

--- myApp.js
+++ myApp.js
@@ -1,3 +1,24 @@
+app.use(function(req, res, next) {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+});
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/json", function(req, res) {
+  let message = "Hello json";
+
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    message = message.toUpperCase();
+  }
+
+  res.json({
+    message: message
+  });
+});
+
 app.get("/now", function(req, res, next) {
   req.time = new Date().toString();
   next();
@@ -7,17 +28,13 @@ app.get("/now", function(req, res, next) {
   });
 });
 
-
 app.get("/:word/echo", function(req, res) {
   res.json({
     echo: req.params.word
   });
 });
 
-
 module.exports = app;
-
-
 
 
 
